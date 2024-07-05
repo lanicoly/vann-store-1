73439fdb53392cd7fb72b8ced987407e4a05bebb
pri <priscilafreitasmartins0@gmail.com>
2024-07-05 11:35:07 -0300

    barra de pesquisa

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function search() {
+    let input = document.getElementById('search-input').value.toLowerCase();
+    let cards = document.getElementsByClassName('card');
+    
+    for (let i = 0; i < cards.length; i++) {
+        let h4 = cards[i].getElementsByTagName('h4')[0];
+        if (h4 && h4.innerHTML.toLowerCase().includes(input)) {
+            cards[i].style.visibility = "visible";
+        } else {
+            cards[i].style.visibility = "hidden";
+        }
+    }
+}
+
+
 $('.carousel').slick({
     dots: true,
     infinite: true,
@@ -5,3 +20,4 @@ $('.carousel').slick({
     slidesToShow: 2,
     slidesToScroll: 2,
     });
+
